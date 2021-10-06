493f775570bcfb809777a4573fc8e8c2d8753fd3
Nikhil Gupta <76448370+Nikhilgupta814@users.noreply.github.com>
2021-10-06 23:38:08 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, request
+from flask.helpers import flash
+import joblib
+
+####  initialize the app ####
+
+app = Flask(__name__)
+model = joblib.load('dib_79.pkl')
+
+@app.route('/')
+def hello_world():
+    return render_template('home.html')
+
+@app.route('/predict', methods =['post'])
+def predict():
+    preg = request.form.get('preg')
+    plas = request.form.get('plas')
+    pres = request.form.get('pres')
+    skin = request.form.get('skin')
+    test = request.form.get('test')
+    mass = request.form.get('mass')
+    pedi = request.form.get('pedi')
+    age = request.form.get('age')
+
+    print(preg, plas, pres, skin, test, mass, pedi, age)
+    output = model.predict([[preg, plas, pres, skin, test, mass, pedi, age]])
+
+    if output[0]==1:
+        ans = 'Diabatic'
+
+    else:
+        ans = 'NOt Diabatic'
+
+    return render_template('predict.html', predict = ans)
+
+#### Run the app ###
+app.run(debug=True)
